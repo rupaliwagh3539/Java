2a7e72fdafc2d0f042daadec7d62999ea6be7a84
Rupali Janardan Wagh <rupalij@maveric-systems.com>
2023-10-04 21:28:14 +0530

    third commit

--- src/com/training/prog/Hello.java
+++ src/com/training/prog/Hello.java
@@ -1,10 +1,10 @@
 package com.training.prog;
 
 public class Hello {
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("hello......");
+
+		System.out.println("java");
 	}
 
 }
